83bffa73fae0020da93310efa695da867d4b32da
Sachin <sachinlakshan04@gmail.com>
2023-12-29 01:53:21 +0530

    hero-styling modifications

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,11 +4,11 @@ import heroImage from "@/public/images/hero.png";
 const Hero = () => {
   return (
     <div className="p-3 bg-slate-50 h-screen lg:flex lg:flex-row-reverse lg:items-center lg:space-x-3">
-      <div className="w-full flex justify-center lg:w-1/3">
+      <div className="w-full flex justify-center lg:w-1/2 xl:w-1/3">
         <Image src={heroImage} alt="hero image" height={400} />
       </div>
-      <div className="max-lg:mt-12 lg:pl-4 lg:w-2/3">
-        <p className="text-3xl md:text-[45px] md:leading-[48px] lg:text-[60px] lg:leading-[60px] max-lg:text-center font-medium">
+      <div className="max-lg:mt-5 lg:pl-4 lg:w-1/2 xl:w-2/3">
+        <p className="text-[36px] leading-[40px] md:text-[45px] md:leading-[48px] lg:text-[60px] lg:leading-[60px] max-lg:text-center font-medium">
           Hi! I'm Sachin. A{" "}
           <span className="text-blue-600 font-semibold">
             Software Engineer{" "}
