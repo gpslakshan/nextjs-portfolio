2b5dcc89078ebddf821361fe80097dba7afa91e5
Sachin <sachinlakshan04@gmail.com>
2023-12-29 17:07:18 +0530

    about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,47 @@
 const About = () => {
-  return <div className="bg-slate-400 h-screen">About</div>;
+  return (
+    <div className="p-6 max-w-[1000px] mx-auto">
+      <h1 className="text-4xl mb-5 lg:mt-[40px] lg:mb-[30px] text-blue-600">
+        About Me
+      </h1>
+      <p className="mb-3 xl:text-lg">
+        Hello there! I'm <strong>Sachin Lakshan</strong>, a forward-thinking
+        <strong> Software Engineer</strong> with a passion for transforming
+        concepts into cutting-edge digital solutions. Armed with a{" "}
+        <strong>BEng (Hons) in Software Engineering</strong> from the{" "}
+        <strong>University of Westminster</strong>, I bring a solid academic
+        foundation and <strong>over 2 years</strong> of hands-on industry
+        experience to the table.
+      </p>
+      <p className="mb-3 xl:text-lg">
+        My journey in software engineering has been a thrilling exploration of
+        technology's vast landscape. From crafting elegant code to designing
+        robust systems, I thrive on the challenges that come with pushing the
+        boundaries of what's possible. Throughout my academic and professional
+        endeavors, I've honed my skills in a variety of technologies and
+        methodologies, ensuring that each project I undertake is not just a task
+        but a chance to innovate.
+      </p>
+      <p className="mb-3 xl:text-lg">
+        Driven by a commitment to excellence, I continuously seek opportunities
+        to stay at the forefront of industry trends. I believe in the power of
+        collaboration, effective communication, and a relentless pursuit of
+        improvement. Whether it's developing intricate web applications or
+        engineering resilient backend systems, I approach every task with a
+        blend of creativity and precision.
+      </p>
+      <p className="mb-3 xl:text-lg">
+        Beyond the lines of code, I am a firm believer in the potential of
+        technology to make a positive impact on the world. In my portfolio,
+        you'll find a curated collection of projects that reflect my dedication
+        to creating meaningful and user-centric solutions.
+      </p>
+      <p className="mb-3 xl:text-lg">
+        Join me on this exciting journey where technology meets innovation, and
+        let's explore the limitless possibilities that lie ahead.
+      </p>
+    </div>
+  );
 };
 
 export default About;
