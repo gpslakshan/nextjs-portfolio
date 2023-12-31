aa39f22fd05766dcffe20321cddf2917a9a6333a
Sachin <sachinlakshan04@gmail.com>
2023-12-31 14:34:02 +0530

    styling modifications for skill card

--- app/components/SkillCard.tsx
+++ app/components/SkillCard.tsx
@@ -1,6 +1,5 @@
 import { SkillData } from "@/types";
 import Image from "next/image";
-import Link from "next/link";
 
 interface Props {
   skillData: SkillData;
@@ -9,7 +8,7 @@ interface Props {
 const SkillCard = ({ skillData }: Props) => {
   return (
     <div className="shadow-lg p-3 bg-slate-200 rounded-lg flex flex-col justify-center items-center w-full">
-      <div className="mb-1 w-full relative h-[170px]">
+      <div className="mb-2 w-full relative h-[170px]">
         <Image
           src={skillData.imageUrl}
           alt={skillData.name}
@@ -17,7 +16,7 @@ const SkillCard = ({ skillData }: Props) => {
           className="object-contain"
         />
       </div>
-      <div className="mb-2">{skillData.name}</div>
+      <div className="">{skillData.name}</div>
     </div>
   );
 };
