251d896e2d4a2bd31f19924e15db5f1795ae5d27
Sachin <sachinlakshan04@gmail.com>
2023-12-31 16:55:14 +0530

    Sidemenu styling bug fix for larger mobile devices.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,12 @@
 const Footer = () => {
-  return <footer className="sm:hidden">Footer</footer>;
+  return (
+    <footer className="lg:hidden">
+      <div className="w-full text-center mb-3">
+        Copyright © {new Date().getFullYear()} Sachin Lakshan. All rights
+        reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -14,6 +15,7 @@ const Home = () => {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 };
